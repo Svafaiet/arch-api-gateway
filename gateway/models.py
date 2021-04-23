b48b25a2e76e19bac384d769bfc62cdd36d08d09
svafaiet <svafaiet@gmail.com>
2021-04-23 19:44:05 +0430

    Seperate microservice repositories

---
+++ gateway/models.py
@@ -0,0 +1,58 @@
+import json
+from datetime import datetime
+
+import requests
+from django.db import models
+
+
+# Create your models here.
+
+
+class API(models.Model):
+    name = models.CharField(max_length=128, blank=True, null=True)
+    url = models.CharField(max_length=128)
+    recent_failure_time = models.DateTimeField(null=True, blank=True)
+    failure_strike = models.IntegerField(default=0)
+
+    def send_request(self, request, path):
+        headers = {'authorization': request.META.get('HTTP_AUTHORIZATION')}
+
+        url = self.url + path
+        method = request.method.lower()
+        method_map = {
+            'get': requests.get,
+            'post': requests.post,
+            'put': requests.put,
+            'delete': requests.delete
+        }
+
+        if request.content_type and request.content_type.lower() == 'application/json':
+            data = json.dumps(request.data)
+            headers['content-type'] = request.content_type
+        else:
+            data = request.data
+
+        try:
+            result = method_map[method](url, headers=headers, data=data, timeout=5)
+            self.failure_strike = 0
+            self.save()
+            if result.status_code / 500 >= 1:
+                raise Exception()
+            elif result.status_code / 400 >= 1:
+                return result
+            elif result.status_code / 300 >= 1:
+                return result
+            else:
+                return result
+        except Exception as e:
+            print(str(e))
+            self.failure_strike += 1
+            self.recent_failure_time = datetime.now()
+            self.save()
+            return -1
+
+    def __unicode__(self):
+        return self.name
+
+    def __str__(self):
+        return self.name
---
+++ gateway/views.py
@@ -0,0 +1,47 @@
+# Create your views here.
+from datetime import datetime
+from functools import reduce
+
+from django.http import HttpResponse
+from rest_framework.exceptions import ValidationError
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from gateway.models import API
+
+
+class MainGatewayView(APIView):
+
+    def redirect(self, request):
+        paths = request.path.split('/')
+        if len(paths) < 2:
+            raise ValidationError('incorrect path format')
+        api = API.objects.filter(name=paths[2])
+        if api.exists():
+            raise ValidationError('multiple api found for this service name')
+        api = api[0]
+        if api.failure_strike >= 3:
+            now = datetime.now()
+            if (api.recent_failure_time - now).total_seconds() < 30:
+                return Response('service unavailable', status=503)
+            api.failure_strike = 0
+            api.save()
+        print(paths, reduce(lambda a, b: f'{str(a)}/{str(b)}', paths[3:]))
+
+        res = api.send_request(request, reduce(lambda a, b: f'{str(a)}/{str(b)}', paths[3:]))
+        if res == -1:
+            return Response('service unavailable', status=503)
+        if res.headers.get('Content-Type', '').lower() == 'application/json':
+            data = res.json()
+        else:
+            data = res.content
+        return Response(data, status=res.status_code)
+
+    def get(self, request):
+        return self.redirect(request)
+
+    def post(self, request):
+        return self.redirect(request)
+
+    def put(self, request):
+        return self.redirect(request)
